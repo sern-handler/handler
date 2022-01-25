e2ee5fa1bfb0bc9e2c1fd0c52a9793a4496561a4
jacoobes <jacobnguyend@gmail.com>
2022-01-24 23:17:22 -0600

    testing it on personal bot

--- src/types/handler/handler.ts
+++ src/types/handler/handler.ts
@@ -1,6 +1,7 @@
 import type { Ok, Result } from 'ts-results';
 import type { Awaitable, Client, Message, MessagePayload} from 'discord.js';
 import type { MessageOptions } from 'child_process';
+import type Sern from '../..';
 
 export type Visibility = "private" | "public"
 export type possibleOutput = string | MessagePayload & MessageOptions
@@ -15,5 +16,3 @@ export enum CommandType {
     TEXT  = 0b00000001 << 0b00000001,
     SLASH = 0b00000001 << 0b00000010,
 }
-
-declare class MsgHandler {}
