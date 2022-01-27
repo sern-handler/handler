4d85c8928a9da9d2f3175a5c50995e94ef1f4616
jacoobes <jacobnguyend@gmail.com>
2022-01-27 11:27:23 -0600

    adding Util import

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Sern } from "./handler/sern/sern";
+import { Utils } from "./handler/utils/preprocessors/args"
 
-
-module.exports = Sern;
-export default Sern;
+module.exports.Sern = Sern;
+module.exports.Utils = Utils;
+export default { Sern, Utils };
