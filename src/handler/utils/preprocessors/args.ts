981c6fb44771badeeb4fc05421a03192da739ffd
jacoobes <jacobnguyend@gmail.com>
2022-01-26 21:38:44 -0600

    parseBool preprocessor

--- src/handler/utils/preprocessors/args.ts
+++ src/handler/utils/preprocessors/args.ts
@@ -11,11 +11,24 @@ export namespace Utils {
      * 
      * @param {string} arg 
      * @param {possibleOutput} onFailure 
-     * @returns {ArgType<number>} Attempts to use Number.parseInt() on `arg`
+     * @returns {ArgType<number>} Attempts to use `Number.parseInt()` on `arg`
      */
     export function parseInt(arg: string, onFailure: possibleOutput): ArgType<number> {
         const val = Number.parseInt(arg);
         return val === NaN ? Err(onFailure) : Ok(val);
+    }
+
+    export function parseBool(
+        arg: string,
+        onFailure: possibleOutput = `Cannot parse ${arg} as a boolean`,
+        regexes : {yesRegex: RegExp, noRegex: RegExp} = {yesRegex : /(yes|y|👍)/gi, noRegex : /(no|n|👎)/gi}   ): ArgType<boolean> {
+        if(arg.match(regexes.yesRegex)) {
+            return Ok(true);
+        }
+        if(arg.match(regexes.noRegex)) {
+            return Ok(false)
+        }
+        return Err(onFailure)        
     }
     
 }
