01308d9cf49a2b5c36a1557e8e98070766c17719
jacoobes <jacobnguyend@gmail.com>
2022-02-06 23:56:12 -0600

    oops, messed up imports

--- src/types/handler/handler.d.ts
+++ src/types/handler/handler.d.ts
@@ -1,19 +1,20 @@
 import type { Option } from 'ts-results';
-import type { CommandInteraction, CommandInteractionOptionResolver, Message, MessagePayload} from 'discord.js';
+import type { CommandInteraction, CommandInteractionOptionResolver, Message, MessagePayload, MessageOptions} from 'discord.js';
 import type { Sern } from '../../handler/sern';
 
 export type Visibility = "private" | "public"
-export type possibleOutput = string | MessagePayload & MessageOptions
+//Anything that can be sent in a `<TextChannel>#send` or `<CommandInteraction>#reply`
+export type possibleOutput = string | MessagePayload & MessageOptions;
 export type Nullable<T> = T | null;
 
 export type delegate = Sern.Module<unknown>["delegate"]
-
 
 /// Thanks @cursorsdottsx
 export type ParseType<T> = {
     [K in keyof T] : T[K] extends unknown ? [k : K, args: T[K] ] : never;
 }[keyof T];
 
+// A Sern.Module will carry a Context Parameter
 export type Context = {
     text : Option<Message>,
     slash : Option<CommandInteraction>
@@ -22,5 +23,5 @@ export interface Arg {
     text : string;
     slash : SlashOptions
 };
-
+// TypeAlias for interaction.options
 export type SlashOptions =  Omit<CommandInteractionOptionResolver, "getMessage" | "getFocused">;
