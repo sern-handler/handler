2e3c65ad096da34f5064d4e994ea11491575c18d
jacoobes <jacobnguyend@gmail.com>
2022-02-15 17:36:52 -0600

    docs(higherorder.ts): ran linter and add documentation to AllTrue

--- src/handler/utilities/higherOrders.ts
+++ src/handler/utilities/higherOrders.ts
@@ -1,11 +1,17 @@
-import type { Message } from "discord.js";
+import type { Message } from 'discord.js';
 
 type MsgFnArgs = [msgOrInter: Message, prefix?: string];
 type MsgFn = (...args: MsgFnArgs) => boolean;
 
-export function AllTrue(...fn : MsgFn[]) {
+/**
+ * 
+ * @param {MsgFn} fn any function that has argument `MsgFnArgs` returning boolean 
+ * @returns {(message: Message, prefix: string) => boolean}
+ */
+export function AllTrue(...fn : MsgFn[]) :
+ (message: Message, prefix: string) => boolean {
     return (message: Message, prefix: string) => {
-        return fn.every(f => f(message, prefix) === true)
-    }
+        return fn.every(f => f(message, prefix) === true);
+    };
 }
 
