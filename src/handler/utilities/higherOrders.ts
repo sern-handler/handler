81921937b53167edf2d91ba1fb9136da1bbade77
jacoobes <jacobnguyend@gmail.com>
2022-02-21 00:41:34 -0600

    refactor: removed options builder as it is redundant

--- src/handler/utilities/higherOrders.ts
+++ src/handler/utilities/higherOrders.ts
@@ -1,5 +1,4 @@
 import type { Message } from 'discord.js';
-import type { OptionData } from '../../types/options';
 
 type MsgFnArgs = [msgOrInter: Message, prefix?: string];
 type MsgFn = (...args: MsgFnArgs) => boolean;
@@ -14,13 +13,4 @@ export function AllTrue(...fns: MsgFn[]):
     return (message: Message, prefix: string) => {
         return fns.every(g => g(message, prefix));
     };
-}
-
-/**
- * A HoF that acts as a ligtweight slash command options builder, enabling user with intellisense
- * @param { T extends keyof OptionData } type type of option
- * @returns { (optionData: Omit<OptionData[T], 'type'>) => ApplicationCommandOptionData | Omit<OptionData[T], 'type'> } creates options
- */
-export function Option<T extends keyof OptionData>(type: T) {
-    return (optionData: Omit<OptionData[T], 'type'>) => { return { type, ...optionData }; };
 }
