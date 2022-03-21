5baff09c03591e52821c036d502c46ccac507760
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2022-03-21 17:27:53 -0500

    feat(types) added util type Override

--- src/types/handler.ts
+++ src/types/handler.ts
@@ -25,3 +25,7 @@ export type DiscordEvent =
     ParseType< { [K in keyof ClientEvents ] : (...args : ClientEvents[K]) => Awaitable<void> }>;
 
 export type SlashOptions = Omit<CommandInteractionOptionResolver, 'getMessage' | 'getFocused'>;
+
+//https://dev.to/vborodulin/ts-how-to-override-properties-with-type-intersection-554l
+export type Override<T1, T2> = Omit<T1, keyof T2> & T2;
+
