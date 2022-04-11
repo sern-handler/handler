1b55d2debcd52cb5ae900a2c74cf9721a0db4f25
EvolutionX <85353424+EvolutionX-10@users.noreply.github.com>
2022-04-11 09:37:26 +0530

    fix: failing test will now no longer fail

--- tests/functions.test.ts
+++ tests/functions.test.ts
@@ -1,4 +1,4 @@
-import { hasPrefix, fmt, isFromBot } from '../src/handler/utilities/messageHelpers';
+import { hasPrefix, fmt, isNotFromBot } from '../src/handler/utilities/messageHelpers';
 describe('FUNCTIONS', () => {
   test('If hasPrefix is a function', () => {
     expect(typeof hasPrefix).toBe('function');
@@ -6,7 +6,7 @@ describe('FUNCTIONS', () => {
   test('if fmt is a function', () => {
     expect(typeof fmt).toBe('function');
   });
-  test('if isBot is a function', () => {
-    expect(typeof isFromBot).toBe('function');
+  test('if isNotFromBot is a function', () => {
+    expect(typeof isNotFromBot).toBe('function');
   });
 });
