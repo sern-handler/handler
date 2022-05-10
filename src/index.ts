61645367ad24bdb1e3cba8ea4766e21354dc0934
xxDeveloper <77380166+Murtatrxx@users.noreply.github.com>
2022-05-10 14:18:10 +0300

    style(index.ts): Added a space

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
  * Copyright (C) 2021 Xx-Mohit-xX
  * This software is licensed under MIT LICENSE 
  * For more information, see README.md and LICENSE.md 
-*/
+ */
 
 export * as Sern from './handler/sern';
 export * from './types/handler';
