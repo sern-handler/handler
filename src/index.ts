6a302c955d9cc160c2b8dbfa0654222111ea9dab
xxDeveloper <77380166+Murtatrxx@users.noreply.github.com>
2022-05-10 14:16:15 +0300

    chore: First License boilerplate preview

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+/* 
+ * Batteries-included, yet powerful discord.is framework
+ * Copyright (C) 2021 Xx-Mohit-xX
+ * This software is licensed under MIT LICENSE 
+ * For more information, see README.md and LICENSE.md 
+*/
+
 export * as Sern from './handler/sern';
 export * from './types/handler';
 export * from './handler/structures/structxports';
