4921e5a292ba59e68af1da76f512682a88fe74ac
xxDeveloper <77380166+Murtatrxx@users.noreply.github.com>
2022-05-15 18:50:41 +0300

    style: Updated license boilerplate

--- src/handler/structures/structxports.ts
+++ src/handler/structures/structxports.ts
@@ -1,12 +1,20 @@
+/*
+ * ---------------------------------------------------------------------
+ *  Copyright (C) 2022 Sern
+ *  This software is licensed under the MIT License.
+ *  See LICENSE.md in the project root for license information.
+ * ---------------------------------------------------------------------
+ */
+
 import Context from './context';
 import type { SlashCommand, TextCommand, BothCommand, Module } from '../structures/modules/commands/module';
 import type Wrapper from './wrapper';
 
 export { 
-    Context,
-    SlashCommand,
-    TextCommand,
-    BothCommand,
-    Module,
-    Wrapper 
+  Context,
+  SlashCommand,
+  TextCommand,
+  BothCommand,
+  Module,
+  Wrapper 
 };
