52510b08ad3b0fa08e45bf72b7b1f32e3d7cf86e
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2022-06-14 14:16:55 -0500

    feat: add events.ts for more customizable event handling

--- src/handler/structures/enums.ts
+++ src/handler/structures/enums.ts
@@ -2,14 +2,17 @@
  * @enum { number };
  */
 enum CommandType {
-    Text = 0b00000001,
-    Slash = 0b00000010,
-    MenuUser = 0b00000100,
-    MenuMsg = 0b00001000,
-    Button = 0b00010000,
-    MenuSelect = 0b00100000,
-    Modal = 0b01000000,
-    Autocomplete = 0b10000000,
+    Text = 0b00000000001,
+    Slash = 0b00000000010,
+    MenuUser = 0b00000000100,
+    MenuMsg = 0b0000001000,
+    Button = 0b00000010000,
+    MenuSelect = 0b00000100000,
+    Modal = 0b00001000000,
+    Autocomplete = 0b00010000000,
+    Discord = 0b00100000000,
+    External = 0b01000000000,
+    Sern = 0b10000000000,
     Both = 0b0000011,
 }
 
