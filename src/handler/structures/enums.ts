ce06e8158a24a5cf522317bf120fc13f13522067
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2022-06-24 21:33:18 -0500

    feat: add enum for event modules

--- src/handler/structures/enums.ts
+++ src/handler/structures/enums.ts
@@ -4,16 +4,18 @@
 enum CommandType {
     Text = 0b00000000001,
     Slash = 0b00000000010,
+    Both = 0b0000011,
     MenuUser = 0b00000000100,
     MenuMsg = 0b0000001000,
     Button = 0b00000010000,
     MenuSelect = 0b00000100000,
     Modal = 0b00001000000,
-    Autocomplete = 0b00010000000,
-    Discord = 0b00100000000,
-    External = 0b01000000000,
-    Sern = 0b10000000000,
-    Both = 0b0000011,
+}
+
+enum EventType {
+    Discord = 0b01,
+    Sern = 0b10,
+    External = 0b11,
 }
 
 enum PluginType {
@@ -21,4 +23,4 @@ enum PluginType {
     Event = 0b10,
 }
 
-export { CommandType, PluginType };
+export { CommandType, PluginType, EventType };
