7294b19f2d3a3e539f087d8121058606fe9c3f34
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2022-07-04 14:51:53 -0500

    feat: create abstract class for all event handlers

---
+++ src/handler/events/ws-handler/eventsHandler.ts
@@ -0,0 +1,11 @@
+import type Wrapper from '../../structures/wrapper';
+import type { Observable } from 'rxjs';
+import { Subject } from 'rxjs';
+
+export abstract class EventsHandler<T> {
+    protected payloadSubject = new Subject<T>();
+    protected abstract observable: Observable<unknown>;
+    protected constructor(protected wrapper: Wrapper) {}
+    protected abstract init(): void;
+    protected abstract setState(state: T): void;
+}
