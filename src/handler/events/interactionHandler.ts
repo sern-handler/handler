84cae8c4c9613015af10ce0c6b8ad97cad5d8d0f
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2022-07-28 18:20:17 -0500

    refactor: destructure and clean namespaces

--- src/handler/events/interactionHandler.ts
+++ src/handler/events/interactionHandler.ts
@@ -59,24 +59,24 @@ export default class InteractionHandler extends EventsHandler<{
 
     override init() {
         this.discordEvent.subscribe({
-            next: interaction => {
-                if (isMessageComponent(interaction)) {
-                    const mod = Files.MessageCompCommands[interaction.componentType].get(
-                        interaction.customId,
+            next: event => {
+                if (isMessageComponent(event)) {
+                    const mod = Files.MessageCompCommands[event.componentType].get(
+                        event.customId,
                     );
-                    this.setState({ event: interaction, mod });
-                } else if (isApplicationCommand(interaction) || isAutocomplete(interaction)) {
+                    this.setState({ event, mod });
+                } else if (isApplicationCommand(event) || isAutocomplete(event)) {
                     const mod =
-                        Files.ApplicationCommands[interaction.commandType].get(
-                            interaction.commandName,
-                        ) ?? Files.BothCommands.get(interaction.commandName);
-                    this.setState({ event: interaction, mod });
-                } else if (isModalSubmit(interaction)) {
+                        Files.ApplicationCommands[event.commandType].get(
+                            event.commandName,
+                        ) ?? Files.BothCommands.get(event.commandName);
+                    this.setState({ event, mod });
+                } else if (isModalSubmit(event)) {
                     /**
                      * maybe move modal submits into message component object maps?
                      */
-                    const mod = Files.ModalSubmitCommands.get(interaction.customId);
-                    this.setState({ event: interaction, mod });
+                    const mod = Files.ModalSubmitCommands.get(event.customId);
+                    this.setState({ event, mod });
                 } else {
                     throw Error('This interaction is not supported yet');
                 }
@@ -96,31 +96,31 @@ export default class InteractionHandler extends EventsHandler<{
         }
     }
 
-    protected processModules(payload: { event: Interaction; mod: CommandModule }) {
-        return match(payload.mod)
+    protected processModules({ mod, event }: { event: Interaction; mod: CommandModule }) {
+        return match(mod)
             .with(
                 { type: P.union(CommandType.Slash, CommandType.Both) },
-                applicationCommandDispatcher(payload.event),
+                applicationCommandDispatcher(event),
             )
             .with(
                 { type: CommandType.Modal },
-                modalCommandDispatcher(payload.event as ModalSubmitInteraction),
+                modalCommandDispatcher(event as ModalSubmitInteraction),
             )
             .with(
                 { type: CommandType.Button },
-                buttonCommandDispatcher(payload.event as ButtonInteraction),
+                buttonCommandDispatcher(event as ButtonInteraction),
             )
             .with(
                 { type: CommandType.MenuSelect },
-                selectMenuCommandDispatcher(payload.event as SelectMenuInteraction),
+                selectMenuCommandDispatcher(event as SelectMenuInteraction),
             )
             .with(
                 { type: CommandType.MenuUser },
-                ctxMenuUserDispatcher(payload.event as UserContextMenuCommandInteraction),
+                ctxMenuUserDispatcher(event as UserContextMenuCommandInteraction),
             )
             .with(
                 { type: CommandType.MenuMsg },
-                ctxMenuMsgDispatcher(payload.event as MessageContextMenuCommandInteraction),
+                ctxMenuMsgDispatcher(event as MessageContextMenuCommandInteraction),
             )
             .otherwise(() => {
                 throw Error(SernError.MismatchModule);
