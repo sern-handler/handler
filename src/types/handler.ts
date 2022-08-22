2b6335fba1b6e690002213e359b1b719a489c619
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2022-08-21 22:46:58 -0500

    feat: removing unused types

--- src/types/handler.ts
+++ src/types/handler.ts
@@ -26,11 +26,6 @@ export type EventInput =
     | { mod: EventModule; absPath: string }[]
     | (() => { mod: EventModule; absPath: string }[]);
 
-export type Reconstruct<T> = T extends Omit<infer O, never> ? O & Reconstruct<O> : T;
-
-export type IsOptional<T> = {
-    [K in keyof T]-?: T[K] extends Required<T>[K] ? false : true;
-};
 
 /**
  * Turns a function with a union of array of args into a single union
@@ -44,7 +39,6 @@ export type SpreadParams<T extends (...args: never) => unknown> = (
  * After modules are transformed, name and description are given default values if none
  * are provided to Module. This type represents that transformation
  */
-export type DefinedModule = DefinitelyDefined<Module, 'name' | 'description'>;
 export type DefinedCommandModule = DefinitelyDefined<CommandModule, 'name' | 'description'>;
 export type DefinedEventModule = DefinitelyDefined<EventModule, 'name' | 'description'>;
 export type Payload =
@@ -53,6 +47,6 @@ export type Payload =
 export type SernEventsMapping = {
     ['module.register']: [Payload];
     ['module.activate']: [Payload];
-    ['error']: [Error | string];
+    ['error']: [message: Error | string, root: Module];
     ['warning']: [string];
 };
