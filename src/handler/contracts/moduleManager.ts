2830370025089f34015a841bcd27457f051cac5b
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2022-08-25 00:17:27 -0500

    feat: getting global plugins some typings and adding contracts

--- src/handler/contracts/moduleManager.ts
+++ src/handler/contracts/moduleManager.ts
@@ -1,5 +1,11 @@
-
+import type {
+    CommandModuleDefs,
+} from '../../types/module';
+import type { CommandType } from '../structures/enums';
 
 interface ModuleManager {
-
+    getModule<T extends CommandType>(type: T) : CommandModuleDefs[T] | undefined
+    setModule<T extends CommandType>(type : T) : void
 }
+
+export default ModuleManager;
