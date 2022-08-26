e2471d32cfc4023ed8915ab05a828bc096a2dc46
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2022-08-26 00:07:05 -0500

    feat: solidifying contract

--- src/types/module.ts
+++ src/types/module.ts
@@ -17,8 +17,12 @@ import type {
     SelectMenuInteraction,
     UserContextMenuCommandInteraction,
 } from 'discord.js';
-import type { DiscordEventCommand, ExternalEventCommand, SernEventCommand } from '../handler/structures/events';
-import { CommandType, PluginType } from '../handler/structures/enums';
+import type {
+    DiscordEventCommand,
+    ExternalEventCommand,
+    SernEventCommand,
+} from '../handler/structures/events';
+import { CommandType } from '../handler/structures/enums';
 import type { Args, Override, SlashOptions } from './handler';
 import type Context from '../handler/structures/context';
 import type { AutocompletePlugin, CommandPlugin, EventPlugin } from '../handler/plugins/plugin';
@@ -39,7 +43,7 @@ export type TextCommand = Override<
         alias?: string[];
         execute: (ctx: Context, args: ['text', string[]]) => Awaitable<void | unknown>;
     }
-    >;
+>;
 
 export type SlashCommand = Override<
     BaseModule,
@@ -50,7 +54,7 @@ export type SlashCommand = Override<
         options?: SernOptionsData[];
         execute: (ctx: Context, args: ['slash', SlashOptions]) => Awaitable<void | unknown>;
     }
-    >;
+>;
 
 export type BothCommand = Override<
     BaseModule,
@@ -62,7 +66,7 @@ export type BothCommand = Override<
         options?: SernOptionsData[];
         execute: (ctx: Context, args: Args) => Awaitable<void | unknown>;
     }
-    >;
+>;
 
 export type ContextMenuUser = Override<
     BaseModule,
@@ -72,7 +76,7 @@ export type ContextMenuUser = Override<
         plugins: CommandPlugin[];
         execute: (ctx: UserContextMenuCommandInteraction) => Awaitable<void | unknown>;
     }
-    >;
+>;
 
 export type ContextMenuMsg = Override<
     BaseModule,
@@ -82,7 +86,7 @@ export type ContextMenuMsg = Override<
         plugins: CommandPlugin[];
         execute: (ctx: MessageContextMenuCommandInteraction) => Awaitable<void | unknown>;
     }
-    >;
+>;
 
 export type ButtonCommand = Override<
     BaseModule,
@@ -92,7 +96,7 @@ export type ButtonCommand = Override<
         plugins: CommandPlugin[];
         execute: (ctx: ButtonInteraction) => Awaitable<void | unknown>;
     }
-    >;
+>;
 
 export type SelectMenuCommand = Override<
     BaseModule,
@@ -102,7 +106,7 @@ export type SelectMenuCommand = Override<
         plugins: CommandPlugin[];
         execute: (ctx: SelectMenuInteraction) => Awaitable<void | unknown>;
     }
-    >;
+>;
 
 export type ModalSubmitCommand = Override<
     BaseModule,
@@ -112,7 +116,7 @@ export type ModalSubmitCommand = Override<
         plugins: CommandPlugin[];
         execute: (ctx: ModalSubmitInteraction) => Awaitable<void | unknown>;
     }
-    >;
+>;
 
 // Autocomplete commands are a little different
 // They can't have command plugins as they are
@@ -126,7 +130,7 @@ export type AutocompleteCommand = Override<
         onEvent: AutocompletePlugin[];
         execute: (ctx: AutocompleteInteraction) => Awaitable<void | unknown>;
     }
-    >;
+>;
 export type EventModule = DiscordEventCommand | SernEventCommand | ExternalEventCommand;
 export type CommandModule =
     | TextCommand
@@ -159,9 +163,6 @@ export type EventModuleDefs = {
     [EventType.External]: ExternalEventCommand;
 };
 
-//TODO: support deeply nested Autocomplete
-// objective: construct union of ApplicationCommandOptionData change any Autocomplete data
-// into Sern autocomplete data.
 
 export type SernAutocompleteData = Override<
     BaseApplicationCommandOptionsData,
@@ -173,7 +174,7 @@ export type SernAutocompleteData = Override<
             | ApplicationCommandOptionType.Integer;
         command: AutocompleteCommand;
     }
-    >;
+>;
 
 /**
  * Type that replaces autocomplete with {@link SernAutocompleteData}
@@ -192,7 +193,7 @@ export type SernSubCommandData = Override<
         type: ApplicationCommandOptionType.Subcommand;
         options?: BaseOptions[];
     }
-    >;
+>;
 
 export type SernSubCommandGroupData = Override<
     Omit<BaseApplicationCommandOptionsData, 'required'>,
@@ -200,11 +201,11 @@ export type SernSubCommandGroupData = Override<
         type: ApplicationCommandOptionType.SubcommandGroup;
         options?: SernSubCommandData[];
     }
-    >;
+>;
 
 export type SernOptionsData<U extends ApplicationCommandOptionData = ApplicationCommandOptionData> =
     U extends ApplicationCommandSubCommandData
         ? SernSubCommandData
         : U extends ApplicationCommandSubGroupData
-            ? SernSubCommandGroupData
-            : BaseOptions;
+        ? SernSubCommandGroupData
+        : BaseOptions;
