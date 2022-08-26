51df5e77efa16988e7a9fbaf38d0057e5ef41531
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2022-08-26 00:07:05 -0500

    feat: solidifying contract

--- src/handler/contracts/moduleManager.ts
+++ src/handler/contracts/moduleManager.ts
@@ -2,10 +2,22 @@ import type {
     CommandModuleDefs,
 } from '../../types/module';
 import type { CommandType } from '../structures/enums';
+import type { CommandModule } from '../../types/module';
+import type { ModuleStore } from '../structures/moduleStore';
+import type { Client } from 'discord.js';
 
-interface ModuleManager {
-    getModule<T extends CommandType>(type: T) : CommandModuleDefs[T] | undefined
-    setModule<T extends CommandType>(type : T) : void
+export abstract class ModuleManager<T extends ModuleStore = ModuleStore> {
+     constructor(
+        protected readonly _client: Client,
+        protected readonly _moduleStore : T
+    ) {}
+    protected abstract getModule<T extends CommandType>(type: T, name: string) : CommandModuleDefs[T] | undefined
+    protected abstract setModule<T extends CommandType>(type : T, value: CommandModule) : void
 }
 
+export interface ModuleManagerConstructor<T extends ModuleStore> {
+    new (client : Client, moduleStore: T) : ModuleManager<T>
+}
 export default ModuleManager;
+
+export type ModuleConfig<T extends ModuleStore = ModuleStore> = ( client : Client ) => ModuleManager<T>
