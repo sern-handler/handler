399ba451d8c239bf82888429adbe3b0a432aafdb
EvolutionX-10 <EvolutionX-10@users.noreply.github.com>
2022-08-29 01:35:11 +0000

    style: pretty please

--- src/handler/utilities/treeSearch.ts
+++ src/handler/utilities/treeSearch.ts
@@ -1,31 +1,40 @@
 import type { SernOptionsData } from '../structures/module';
 import { ApplicationCommandOptionType, AutocompleteInteraction } from 'discord.js';
 
-export default function treeSearch(iAutocomplete: AutocompleteInteraction, options: SernOptionsData[] | undefined) {
-    if(options === undefined) return undefined;
+export default function treeSearch(
+    iAutocomplete: AutocompleteInteraction,
+    options: SernOptionsData[] | undefined,
+) {
+    if (options === undefined) return undefined;
     const _options = options.slice(); // required to prevent direct mutation of options
-    while ( _options.length > 0 ) {
+    while (_options.length > 0) {
         const cur = _options.pop()!;
-        switch ( cur.type ) {
-            case ApplicationCommandOptionType.Subcommand : {
-                for ( const option of cur.options ?? [] ) {
-                    _options.push(option);
-                }
-            } break;
-            case ApplicationCommandOptionType.SubcommandGroup : {
-                for (const command of cur.options ?? []) {
-                    _options.push(command);
-                }
-            } break;
-            default : {
-                if(cur.autocomplete) {
-                    const choice = iAutocomplete.options.getFocused(true);
-                    if(cur.name === choice.name && cur.autocomplete) {
-                        return cur;
+        switch (cur.type) {
+            case ApplicationCommandOptionType.Subcommand:
+                {
+                    for (const option of cur.options ?? []) {
+                        _options.push(option);
                     }
-                        return undefined;
                 }
-            } break;
+                break;
+            case ApplicationCommandOptionType.SubcommandGroup:
+                {
+                    for (const command of cur.options ?? []) {
+                        _options.push(command);
+                    }
+                }
+                break;
+            default:
+                {
+                    if (cur.autocomplete) {
+                        const choice = iAutocomplete.options.getFocused(true);
+                        if (cur.name === choice.name && cur.autocomplete) {
+                            return cur;
+                        }
+                        return undefined;
+                    }
+                }
+                break;
         }
     }
 }
