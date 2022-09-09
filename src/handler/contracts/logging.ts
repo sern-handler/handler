13cdb8b8cb6eeba8a157ea905087a43b531e16ae
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2022-09-09 02:06:11 -0500

    remove: ModuleConfiguration

--- src/handler/contracts/logging.ts
+++ src/handler/contracts/logging.ts
@@ -2,12 +2,12 @@ import type { SernEventsMapping } from '../../types/handler';
 import type { ScopedPlugin } from '../../types/handler';
 
 export interface Logging extends ScopedPlugin {
-    error(...payload : SernEventsMapping['error']) : void;
-    warning(...payload : SernEventsMapping['warning']) : void;
-    moduleActivate(...warning : SernEventsMapping['module.activate']) : void;
-    moduleRegister(...register : SernEventsMapping['module.register']) : void;
+    error(...payload: SernEventsMapping['error']): void;
+    warning(...payload: SernEventsMapping['warning']): void;
+    moduleActivate(...warning: SernEventsMapping['module.activate']): void;
+    moduleRegister(...register: SernEventsMapping['module.register']): void;
 }
 
 export interface LoggingConstructor {
-    new() : Logging
+    new (): Logging;
 }
