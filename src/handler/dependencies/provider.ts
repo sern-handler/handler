2a36569181dc6c25f84166115327a583bd9b76f0
jacoobes <jacobnguyend@gmail.com>
2022-09-16 01:25:57 -0500

    feat: add POC for sern di IOC

--- src/handler/dependencies/provider.ts
+++ src/handler/dependencies/provider.ts
@@ -1,12 +1,28 @@
 import type { NodeApi } from 'iti';
 import { SernError } from '../structures/errors';
+import { BehaviorSubject } from 'rxjs';
+import { Err, Ok } from 'ts-results-es';
+import { makeRoot } from 'iti';
 
+export const containerSubject = new BehaviorSubject<NodeApi<Record<string,unknown>> | null>(null);
 export function requireDependencies(root: NodeApi<Record<string,unknown>>) {
     const tokens = Object.values(root.getTokens());
 
     const hasRequiredDependencies = tokens.includes('@sern/client');
     if (!hasRequiredDependencies) {
-        throw Error(SernError.RequiredNotFound);
+        return Err(SernError.RequiredNotFound);
     }
+    return Ok(root);
+}
 
+export function useContainer() {
+    const container = containerSubject.getValue();
+    if(container === null) throw Error('useContainer was called before Sern#init');
+    return {
+        container,
+    };
+}
+
+export function makeDependencies() {
+    return makeRoot();
 }
