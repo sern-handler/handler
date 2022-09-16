766d86f72f616e3bc4e1484c64efa3683408dd37
jacoobes <jacobnguyend@gmail.com>
2022-09-16 01:28:13 -0500

    revert: remove old code

--- src/handler/structures/defaultModuleManager.ts
+++
@@ -1,5 +0,0 @@
-import ModuleManager from '../contracts/moduleManager';
-import type { CommandModule, CommandModuleDefs } from '../../types/module';
-import type { CommandType } from './enums';
-import { ModuleStore } from './moduleStore';
-import { Logging, LoggingConstructor } from '../contracts/logging';
