74a5726970e3f9050183dcda2217c015890ddfb6
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2023-01-10 17:49:48 -0600

    revert: remove unneeded function

--- src/handler/utilities/arrAsync.ts
+++
@@ -1,9 +0,0 @@
-import type { Awaitable } from 'discord.js';
-
-export async function arrAsync<T>(promiseLike: Awaitable<T>[]): Promise<T[]> {
-    const arr: T[] = [];
-    for await (const el of promiseLike) {
-        arr.push(el);
-    }
-    return arr;
-}
