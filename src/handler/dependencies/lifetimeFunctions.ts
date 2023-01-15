d46e3c58b0f57d0f68cd424adbee84775de8797d
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2023-01-15 09:12:44 -0600

    feat: help mitigate breaking changes

--- src/handler/dependencies/lifetimeFunctions.ts
+++ src/handler/dependencies/lifetimeFunctions.ts
@@ -1,8 +1,17 @@
-
-export function single<T>(cb: () => T) {
-    return cb;
+/**
+ * Following iti's singleton and transient implementation,
+ * use single if you want a singleton object
+ * @param cb
+ */
+export function single<T>(cb: (() => T) | T) {
+    if(typeof cb === 'function')
+        return cb;
 }
-
+/**
+ * Following iti's singleton and transient implementation,
+ * use transient if you want a new dependency every time your container getter is called
+ * @param cb
+ */
 export function transient<T>(cb: () => () => T) {
     return cb;
 }
