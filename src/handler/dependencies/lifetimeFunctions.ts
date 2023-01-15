c3e55a7af6cf2c70dbc488815959435c510f1576
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2023-01-15 10:44:21 -0600

    feat: help mitigate breaking changes and function overloads

--- src/handler/dependencies/lifetimeFunctions.ts
+++ src/handler/dependencies/lifetimeFunctions.ts
@@ -1,25 +1,34 @@
+type NotFunction = string | number | boolean | null | undefined | bigint |
+    readonly any[] | { apply?: never, [k: string]: any } |
+    { call?: never, [k: string]: any };
 
 /**
  * @deprecated
  * @param cb
- * Deprecated signature: please pass a callback instead of just a dependency
  */
-export function single<T>(cb: T) : T
+export function single<T extends NotFunction>(cb: T) : () => T;
 /**
- * Following iti's singleton and transient implementation,
- * use single if you want a singleton object
+ * New signature
  * @param cb
  */
-export function single<T>(cb: (() => T) | T) {
-    if(typeof cb === 'function')
-        return cb;
+export function single<T extends () => unknown>(cb: T) : T;
+/**
+ * Please note that on intellij, the deprecation is for all signatures, which is unintended behavior (and
+ * very annoying).
+ * For future versions, ensure that single is being passed as a **callback!!**
+ * @param cb
+ */
+export function single<T>(cb: T) {
+   if(typeof cb === 'function') return cb;
+   return () => cb;
 }
 /**
  * @deprecated
  * @param cb
  * Deprecated signature
  */
-export function transient<T>(cb: T) : () => () => T
+export function transient<T extends NotFunction>(cb: T) : () => () => T
+export function transient<T extends () => () => unknown>(cb: T) : T;
 /**
  * Following iti's singleton and transient implementation,
  * use transient if you want a new dependency every time your container getter is called
