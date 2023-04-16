bc4802f2613c61d1da8ade1b43d49a5e2121fb08
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2023-04-16 13:27:17 -0500

    chore:remove const function

--- src/core/utilities/functions.ts
+++ src/core/utilities/functions.ts
@@ -2,14 +2,6 @@ import * as Files from '../module-loading/readFile';
 import { basename } from 'path';
 import { Err, Ok } from 'ts-results-es';
 /**
- * A function that returns whatever value is provided.
- * Warning: this evaluates { @param value }. It does not defer a value.
- * @param value
- * @__PURE__
- */
-// prettier-ignore
-export const _const = <T>(value: T) => () => value;
-/**
  *
  * @param modName
  * @param absPath
@@ -19,8 +11,8 @@ export function nameOrFilename(modName: string | undefined, absPath: string) {
 }
 
 //function wrappers for empty ok / err
-export const ok = _const(Ok.EMPTY);
-export const err = _const(Err.EMPTY);
+export const ok = () => Ok.EMPTY;
+export const err = () => Err.EMPTY;
 
 export function partition<T, V>(arr: (T & V)[], condition: (e: T & V) => boolean): [T[], V[]] {
     const t: T[] = [];
