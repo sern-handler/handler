d41cc9ea2d6f6d51cf3c60fea5cc37e3d14b66c5
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2023-04-27 13:20:11 -0500

    move strategy to index.ts and add adapters

--- src/core/platform/index.ts
+++ src/core/platform/index.ts
@@ -1,1 +1,38 @@
-export * from './strategy';
+export const enum DispatchType {
+    Websocket,
+    Serverless
+}
+
+export type PlatformStrategy =
+    | WebsocketStrategy
+    | ServerlessStrategy;
+
+export interface WebsocketStrategy {
+    type: DispatchType.Websocket;
+    //icreate, messageCreate, ready
+    eventNames: [ string, string, string]
+    defaultPrefix?: string;
+}
+
+export interface ServerlessStrategy {
+    type: DispatchType.Serverless;
+    endpoint: string;
+}
+
+export function makeWebsocketAdapter(
+    eventNames: [interactionCreate: string, messageCreate: string, ready: string],
+    defaultPrefix?: string
+): WebsocketStrategy {
+    return {
+        type: DispatchType.Websocket,
+        eventNames,
+        defaultPrefix
+    };
+}
+
+export function makeServerlessAdapter(i : { endpoint: string }): ServerlessStrategy {
+    return {
+        type: DispatchType.Serverless ,
+        ...i
+    };
+}
