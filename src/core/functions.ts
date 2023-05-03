a5e8743afd66871b8cb2df1919fcd97bb65f8f71
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2023-05-03 13:00:11 -0500

    chore: remove utils folder in favor of single file

--- src/core/functions.ts
+++ src/core/functions.ts
@@ -1,12 +1,32 @@
+import { Err, Ok } from 'ts-results-es';
 import { ApplicationCommandOptionType, AutocompleteInteraction } from 'discord.js';
 import type { SernAutocompleteData, SernOptionsData } from '../../types/module';
+
+
+//function wrappers for empty ok / err
+export const ok = () => Ok.EMPTY;
+export const err = () => Err.EMPTY;
+
+export function partition<T, V>(arr: (T & V)[], condition: (e: T & V) => boolean): [T[], V[]] {
+    const t: T[] = [];
+    const v: V[] = [];
+    for (const el of arr) {
+        if (condition(el)) {
+            t.push(el as T);
+        } else {
+            v.push(el as V);
+        }
+    }
+    return [t, v];
+}
+
 
 /**
  * Uses an iterative DFS to check if an autocomplete node exists
  * @param iAutocomplete
  * @param options
  */
-export default function treeSearch(
+export function treeSearch(
     iAutocomplete: AutocompleteInteraction,
     options: SernOptionsData[] | undefined,
 ): SernAutocompleteData | undefined {
