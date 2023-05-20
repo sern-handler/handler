a6db21c7206a94c26eb8e93a1351f7c905f5e88f
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2023-05-19 20:33:11 -0500

    docs: add purpose of d.ts file

--- src/core/ioc/dependencies.d.ts
+++ src/core/ioc/dependencies.d.ts
@@ -1,3 +1,9 @@
+// This file serves an the interface for developers to augment the Dependencies interface
+// Developers will have to create a new file dependencies.d.ts in the root directory, augmenting 
+// this type
+
+/* eslint-disable @typescript-eslint/consistent-type-imports */
+
 import { CoreDependencies } from './types'
 
 declare global {
