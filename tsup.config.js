5c2e8387630137dc9065afae75362fd9b432fd05
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2023-08-17 10:37:54 -0500

    fix regression of context and fix tsup build

--- tsup.config.js
+++ tsup.config.js
@@ -17,35 +17,8 @@ export default defineConfig([
         target: 'node18',
         tsconfig: './tsconfig.json',
         outDir: './dist',
-        splitting: true,
-        dts: true,
         minify: false,
-        bundle: false,
+        dts: true,
         ...shared,
     },
-    //    {
-    //        format: 'cjs',
-    //        esbuildPlugins: [ifdefPlugin({ variables: { MODE: 'cjs' }, verbose: true })],
-    //        splitting: false,
-    //        target: 'node18',
-    //        tsconfig: './tsconfig-cjs.json',
-    //        outDir: './dist/cjs',
-    //        outExtension() {
-    //            return {
-    //                js: '.cjs',
-    //            };
-    //        },
-    //        async onSuccess() {
-    //            console.log('writing json commonjs');
-    //            await writeFile('./dist/cjs/package.json', JSON.stringify({ type: 'commonjs' }));
-    //        },
-    //        ...shared,
-    //    },
-    //    {
-    //        dts: {
-    //            only: true,
-    //        },
-    //        entry: ['src/index.ts'],
-    //        outDir: 'dist',
-    //    },
-]);
+ ]);
