a5ff32a347597f8bf8e8ed90904e64fa506e2a7d
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2023-12-27 01:12:16 -0600

    refine presence api

--- src/core/presences.ts
+++ src/core/presences.ts
@@ -4,11 +4,13 @@ import type { Emitter } from "./contracts/emitter";
 
 type Status = 'online' | 'idle' | 'invisible' | 'dnd'
 
-interface Once { 
+export interface Result { 
     status?: Status;
     afk?: boolean;
     activities?: ActivitiesOptions[];
     shardId?: number[];
+    repeat?: number | [Emitter, string];
+    onRepeat?: (previous: Result) => Result; 
 }
 
 export type Config <T extends (keyof Dependencies)[]> = 
@@ -17,17 +19,52 @@ export type Config <T extends (keyof Dependencies)[]> =
     execute: (...v: IntoDependencies<T>) => Result;
 };
 
-export function create<T extends (keyof Dependencies)[]>
+/**
+  * A small wrapper to provide type inference.
+  * Create a Presence module which **MUST** be put in a file called presence.<file-ending> 
+  * adjacent to the file where **Sern.init** is CALLED.
+  */
+export function module<T extends (keyof Dependencies)[]>
 (conf: Config<T>) {
     return conf;
 }
 
-export type Result =
-    | {
-        repeated: { 
-            create: | number 
-                    | [Emitter, string];
-            onInterval: (previous: Result) => Once 
-        }
-      }
-    | Once;
+type PresenceReduce = (previous: Result) => Result;
+
+/**
+  * Create a Presence body which can be either: 
+  * - once, the presence is activated only once.
+  * - repeated, per cycle or event, the presence can be changed.
+  */
+export function of(root: Omit<Result, 'repeat' | 'onRepeat'>) {
+    return { 
+        /**
+         * @example
+         * Presence
+         *     .of({ 
+         *          activities: [{ name: "deez nuts" }] 
+         *     }) //starts the presence with "deez nuts".
+         *     .repeated(prev => { 
+         *         return {
+         *             afk: true,
+         *             activities: prev.activities?.map(s => ({ ...s, name: s.name+"s" }))
+         *         };
+         *     }, 10000)) //every 10 s, the callback sets the presence to the returned one.
+         */
+        repeated: (onRepeat: PresenceReduce, repeat: number | [Emitter, string]) => {
+            return { repeat, onRepeat, ...root }
+        },
+        /**
+         * @example
+         * Presence
+         *     .of({
+         *        activities: [
+         *          { name: "Chilling out" }
+         *        ]
+         *      })
+         *     .once() // Sets the presence once, with what's provided in '.of()'
+         */
+        once: () => root
+    };
+}
+
--- src/handlers/presence.ts
+++ src/handlers/presence.ts
@@ -2,19 +2,22 @@ import { concatMap, from, interval, of, map, scan, startWith, fromEvent } from "
 import { Files } from "../core/_internal";
 import * as Presence from "../core/presences";
 import { Services } from "../core/ioc";
+import assert from "node:assert";
 
 type SetPresence = (conf: Presence.Result) => Promise<unknown>
 
 const parseConfig = async (conf: Promise<Presence.Result>) => {
     return conf
         .then(s => {
-            if('repeated' in s) {
-                const { create, onInterval } = s.repeated;
-                const src$ = typeof create === 'number' 
-                    ? interval(create)
-                    : fromEvent(...create);
+            if('repeat' in s) {
+                const { onRepeat, repeat } = s;
+                assert(repeat !== undefined, "repeat is undefined");
+                assert(onRepeat !== undefined, "onRepeat callback is undefined, but repeat exists");
+                const src$ = typeof repeat === 'number' 
+                    ? interval(repeat)
+                    : fromEvent(...repeat);
                     return src$
-                        .pipe(scan(onInterval, s), 
+                        .pipe(scan(onRepeat, s), 
                               startWith(s));
                 }
             //take 1?
